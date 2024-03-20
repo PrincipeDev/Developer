5aebb422aaa8096dc6e442641dc477512f13c2be
José Borges Costa <borgesdevfront@gmail.com>
2024-03-20 10:52:31 -0300

    Aula 2 comentário

---
+++ JavaScript-Course/Lessons/Section001/lesson002.js
@@ -0,0 +1,4 @@
+// Comentários
+//O que são comentários? são anotações no codigo que serão ignorados pelo motor do javascript.
+// Utilizamos "//" para fazer comentários de uma linha.
+// Utilizamos "/*" para abrir o comentário de multiplas linhas e "*/" para fechar o comentário.
